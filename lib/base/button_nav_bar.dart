9ef73c19658653ac80f3f5b6824cda63872140b2
FUSEINI ABDUL WAHAB <abdulwahabfuseini78@gmail.com>
2025-07-10 17:04:52 +0000

    Created AppBar and BottomNavigation

---
+++ lib/base/button_nav_bar.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_demo/screens/home_screen.dart';
+import 'package:flutter_app_demo/screens/profile_screen.dart';
+import 'package:flutter_app_demo/screens/search_screen.dart';
+import 'package:flutter_app_demo/screens/ticket_screen.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  final appScreen = [
+    const HomeScreen(),
+    const SearchScreen(),
+    const TicketScreen(),
+    const ProfileScreen(),
+  ];
+
+  int _selectedItem = 0;
+
+  void onItemTapped(int index){
+    setState(() {
+      _selectedItem = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Ticket"),
+        leading: IconButton(icon: Icon(Icons.menu), onPressed: (){},),
+        actions: [
+          IconButton(icon: Icon(Icons.notifications), onPressed: (){},),
+          IconButton(icon: Icon(Icons.search), onPressed: (){},),
+        ],
+        elevation: 5,
+        titleSpacing: 5,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Colors.green, Colors.red
+            ],
+            begin: Alignment.topRight,
+              end: Alignment.bottomLeft
+            )
+          ),
+        ),
+      ),
+      body: appScreen[_selectedItem],
+      bottomNavigationBar:
+
+      BottomNavigationBar(
+          onTap: onItemTapped,
+          currentIndex: _selectedItem,
+          selectedItemColor: Colors.green,
+          unselectedItemColor: Colors.black,
+          showUnselectedLabels: true,
+          items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), activeIcon: Icon(Icons.home_filled), label: "Home"),
+        BottomNavigationBarItem(icon: Icon(Icons.search_outlined), activeIcon: Icon(Icons.search), label: "Search"),
+        BottomNavigationBarItem(icon: Icon(Icons.airplane_ticket_outlined), activeIcon: Icon(Icons.airplane_ticket), label: "Home"),
+        BottomNavigationBarItem(icon: Icon(Icons.person_2_outlined), activeIcon: Icon(Icons.person), label: "Home"),
+      ]),
+    );
+  }
+}
