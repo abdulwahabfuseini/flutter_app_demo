b34a3fae33ad9b673c26c9589d140c8e0dee2dc4
FUSEINI ABDUL WAHAB <abdulwahabfuseini78@gmail.com>
2025-07-16 04:19:32 +0000

    Added Quick Access Widget

--- lib/view/screens/home/home_screen.dart
+++ lib/view/screens/home/home_screen.dart
@@ -15,42 +15,120 @@ class HomeScreen extends StatelessWidget {
           child: Image.asset("assets/images/Logo.png", height: 160, width: 160),
         ),
         title: Center(
-          child: RichText(text: const TextSpan(
-            style: TextStyle(fontSize: 20, color: Colors.black, wordSpacing: 4),
-            children: [
-              TextSpan(text: "Yo!", style: TextStyle(fontWeight: FontWeight.bold)),
-              TextSpan(text: " ABDUL", style: TextStyle()),
-            ]
-          )),
+          child: RichText(
+            text: const TextSpan(
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.black,
+                wordSpacing: 4,
+              ),
+              children: [
+                TextSpan(
+                  text: "Yo!",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                TextSpan(
+                  text: " ABDUL",
+                  style: TextStyle(fontWeight: FontWeight.w300),
+                ),
+              ],
+            ),
+          ),
         ),
         backgroundColor: kMtnYellow,
-        actions: [
-          Image.asset("assets/images/bot.png", height: 40, width: 80)
-        ],
+        actions: [Image.asset("assets/images/bot.png", height: 40, width: 80)],
       ),
-      body: LayoutBuilder(builder: (context, constraints) {
-        return SingleChildScrollView(
-          child: ConstrainedBox(constraints: BoxConstraints(
-            maxHeight: constraints.maxHeight
-          ),
-          child: Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(topRight: Radius.circular(30)),
-              color: kLightGrey
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                children: [
-                   headerTitle(text: 'Balances', icon: Icons.refresh,  onViewAll: () {  })
-                ],
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(maxHeight: constraints.maxHeight),
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(30),
+                  ),
+                  color: kLightGrey,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      headerTitle(
+                        text: 'Balances',
+                        icon: Icons.refresh,
+                        onViewAll: () {},
+                      ),
+                      const SizedBox(height: 10),
+                      GridView.count(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 20,
+                        childAspectRatio: 1.6,
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        children: [
+                          balanceCard(
+                            Icons.phone_in_talk_sharp,
+                            "AIRTIME",
+                            "GHS 0.00",
+                            "BONUS: GHS 96.31",
+                          ),
+                          balanceCard(
+                            Icons.mobiledata_off,
+                            "DATA",
+                            "8.61 GB",
+                            "BONUS:",
+                          ),
+                          balanceCard(
+                            Icons.mail_outline_sharp,
+                            "SMS",
+                            "30 SMS",
+                            "BONUS:",
+                          ),
+                          balanceCard(
+                            Icons.wifi,
+                            "BROADBAND",
+                            "GET CONNECTED",
+                            "CLICK HERE",
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 40),
+                      Center(
+                        child: Text(
+                          "Showing balances as at\n Jul 16 2025, 00:05:25",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 40),
+                      headerTitle(text: "Quick access", onViewAll: () {}),
+                      GridView.count(crossAxisCount: 4,
+                      mainAxisSpacing: 5,
+                        crossAxisSpacing: 10,
+                        childAspectRatio: 1.2,
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        children: [
+                          quickAccessCard(Icons.mobiledata_off, "Data Bundle"),
+                          quickAccessCard(Icons.mobiledata_off, "Just4U"),
+                          quickAccessCard(Icons.storage, "Mashup"),
+                          quickAccessCard(Icons.perm_contact_calendar_outlined, "Contact us"),
+                        ],
+                      )
+
+                    ],
+                  ),
+                ),
               ),
             ),
-          ),
-          ),
-        );
-      },),
+          );
+        },
+      ),
     );
   }
 }
--- lib/view/screens/home/home_widgets.dart
+++ lib/view/screens/home/home_widgets.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../../../utils/constants.dart';
 
-Widget headerTitle({ required String text,
+Widget headerTitle({
+  required String text,
   required VoidCallback onViewAll,
-  IconData? icon,}) {
+  IconData? icon,
+}) {
   return Row(
     mainAxisAlignment: MainAxisAlignment.spaceBetween,
     children: [
@@ -14,8 +17,7 @@ Widget headerTitle({ required String text,
             text,
             style: TextStyle(fontWeight: FontWeight.w700, fontSize: 17),
           ),
-          if (icon != null)
-            Icon(icon, size: 20, color: Colors.black54),
+          if (icon != null) Icon(icon, size: 20, color: Colors.black54),
         ],
       ),
       TextButton(
@@ -25,9 +27,13 @@ Widget headerTitle({ required String text,
           children: [
             Text(
               "View All",
-              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 15, color: Colors.black),
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 15,
+                color: Colors.black,
+              ),
             ),
-            Icon(CupertinoIcons.chevron_right_circle_fill, color: Colors.black,),
+            Icon(CupertinoIcons.chevron_right_circle_fill, color: Colors.black),
           ],
         ),
       ),
@@ -35,10 +41,72 @@ Widget headerTitle({ required String text,
   );
 }
 
-
-Widget balanceGridBox(IconData icon, String title, String value, String bonus){
-  return Column(
-
+Widget balanceCard(IconData icon, String title, String value, String bonus) {
+  return Card(
+    color: kMtnYellow,
+    clipBehavior: Clip.antiAlias,
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+    elevation: 3,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
+          child: Row(
+            children: [
+              Icon(icon, size: 20,),
+              const SizedBox(width: 8),
+              Text(
+                title,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(1.5),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [Text(value, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),), Text(bonus, style: TextStyle(fontWeight: FontWeight.w400),)],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    ),
   );
 }
 
+
+Widget quickAccessCard(IconData icon, String value){
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 4, vertical: 6),
+    decoration: BoxDecoration(
+      color: Colors.black,
+      borderRadius: BorderRadius.circular(10)
+    ),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+
+      spacing: 4,
+      children: [
+        Icon(icon, color: Colors.white, size: 30,),
+        Text(value, style: TextStyle(fontWeight: FontWeight.w400, color: Colors.white, fontSize: 13),)
+      ],
+    ),
+  );
+}
